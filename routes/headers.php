a610f0c84421bca05c8a34699fd6739f4a1f347b
rummik <r@9k1.us>
2015-01-15 14:37:51 -0500

    Make it work with PHP in FastCGI mode, because the standard library varies depending on how you execute your code

--- routes/headers.php
+++ routes/headers.php
@@ -1,5 +1,16 @@
 <?php
 	$app->get('/head(ers)?', function() use ($app) {
 		$app->response->headers->set('Content-Type', 'application/json');
-		print json_encode(getallheaders());
+
+		$headers = [];
+
+		foreach ($_SERVER as $key => $value) {
+			if (substr($key, 0, 5) != 'HTTP_')
+				continue;
+
+			$key = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
+			$headers[$key] = $value;
+		}
+
+		print json_encode($headers);
 	});
